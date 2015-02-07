c6e9f1924be11e70db0eba995c9034a13170a61f
Serubin <Serubin@Serubin.net>
2015-02-07 18:30:21 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Solomon Rubin - Serubin.net</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.scrollgress.min.js"></script>
+		<script src="js/jquery-scrollspy.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+			<link rel="stylesheet" href="css/style-noscript.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="index">
+	
+		<!-- Header -->
+		<div id="welcome">
+			<header id="header" class="alt">
+				<h1 id="logo"><a href="./" class="logo"><span>S</span>erubin.net</a></h1>
+				<nav id="nav">
+					<ul>
+						<li id="welcome-nav"><a class="navscroll" href="#welcome">Welcome</a>
+							<ul>
+								<li><a class="scrolly" href="#main">Who Am I?</a></li>
+							</ul>
+						</li>
+						<li id="features-nav"><a class="navscroll" href="#features">Features</a></li>
+						<li id="experience-nav"><a class="navscroll" href="#experience">Experience</a></li>
+						<li id="examples-nav"><a class="navscroll" href="#examples">Examples</a></li>
+						<li id="contact-nav"><a class="navscroll" href="#contact">Contact</a></li>
+						<li><a href="http://secure.serubin.net/login/" class="button special">Login</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->		
+			<section id="banner">
+				
+				<!--
+					".inner" is set up as an inline-block so it automatically expands
+					in both directions to fit whatever's inside it. This means it won't
+					automatically wrap lines, so be sure to use line breaks where
+					appropriate (<br />).
+				-->
+				<div class="inner">
+					
+					<header>
+						<h2 class="logo"><span>S</span>erubin.net</h2>
+					</header>
+					<p>A portfolio for a young, up and coming <br/>
+						<b>System Administrator</b> <br />
+						and <br/>
+						<b>Web Developer</b></p>
+					<footer>
+						<ul class="buttons vertical">
+							<li><a href="#main" class="button fit scrolly">Who am I?</a></li>
+						</ul>
+					</footer>
+				
+				</div>
+				
+			</section>
+		</div>
+		<!-- Main -->
+			<article id="main">
+
+				<header class="special container" id="whoami">
+					<span class="icon circle serubin"></span>
+					<h2>Solomon Rubin<br />
+						-- <br/>
+						Who am I?
+					</h2>
+					<p>I'm currently a student at Rochester Institute of Technology majoring in </br>
+					<b>Computer Science</b>. Over the last 5 years I've made my speciality <b>Web Development</b> and <br/>
+					<b>System Administration</b>. I've had a focus in <b>PHP</b> and <b>Javascript</b> to power<br/>
+					 my web infrastructure designs as they are crucial in any modern website. In addition, I've also worked <br/>
+					 to create a skill set in <b>Linux Administrator</b>.
+				</header>
+					
+				<!-- Features -->
+				<div id="features">
+					<section class="wrapper style2 container special-alt">
+						<div class="row 50%">
+							<div class="8u 12u(2)">
+							
+								<header>
+									<h2><b>Brilliant</b> Features</h2>
+								</header>
+								<p>My work is tailored for what you need. Providing everything from speed to security I'll provide exactly what is needed in short periods of time. Customizability is first priority to ensure that you not only get exactly what you need but also to ensure that my work can grow and change as you do.</p>
+								<footer>
+									<ul class="buttons">
+										<li><a href="#experience" class="button navscroll">Find Out More</a></li>
+									</ul>
+								</footer>
+							
+							</div>
+							<div class="4u 12u(2) important(2)">
+							
+								<ul class="featured-icons">
+									
+									<li><span class="icon fa-lock"><span class="label">Secure</span></span></li>
+									<li><span class="icon fa-paper-plane"><span class="label">Fast Load Times</span></span></li>
+									<li><span class="icon fa-cloud"><span class="label">Cloud Based</span></span></li>
+									<li><span class="icon fa-cog"><span class="label">Customizable</span></span></li>
+									<li><span class="icon fa-mobile"><span class="label">All Platforms</span></span></li>
+									<li><span class="icon fa-users"><span class="label">Expandable</span></span></li>
+									
+								</ul>
+							
+							</div>				
+						</div>
+					</section>
+				</div>
+				<!-- Experience  -->
+				<div id="experience">
+					<section class="wrapper style1 container special">
+						<div class="row">
+							<div class="4u 12u(2)">
+							
+								<section>
+									<span class="icon featured fa-folder"></span>
+									<header>
+										<h3>Linux Administration</h3>
+									</header>
+									<ul>
+										<li>4+ years experience</li>
+										<li>Apache</li>
+										<li>MySQL</li>
+										<li>Bash scripting</li>
+									</ul>
+								</section>
+							
+							</div>
+							<div class="4u 12u(2)">
+							
+								<section>
+									<span class="icon featured fa-cloud"></span>
+									<header>
+										<h3>Web Programming</h3>
+									</header>
+									<ul>
+										<li>5+ years of experience</li>
+										<li>Reusable Frameworks</li>
+										<li>PHP</li>
+										<li>JavaScript</li>
+										<li>HTML/CSS</li>
+									</ul>
+								</section>
+							
+							</div>
+							<div class="4u 12u(2)">
+							
+								<section>
+									<span class="icon featured fa-check"></span>
+									<header>
+										<h3>Employable</h3>
+									</header>
+									<ul>
+										<li>Looking for short and long term employment</li>
+										<li>Excellent Reputation</li>
+										<li>Excellent Communication Skills</li>
+										<li>Excited and Motivated</li>
+									</ul>
+								</section>
+							
+							</div>
+						</div>
+					</section>
+				</div>
+					
+				<!-- Examples -->
+				<div id="examples">
+					<section class="wrapper style3 container special">
+						<header class="major">
+							<h2>Some <strong>Examples</strong></h2>
+						</header>
+						
+						<div class="row">
+							<div class="6u 12u(2)">
+							
+								<section>
+									<a href="#" class="image featured"><img src="images/projects/serubin.net.png" alt="" /></a>
+									<header>
+										<h3>Serubin.net</h3>
+									</header>
+									<p>Serubin.net has been much more than a project. It's been a learning tool for many years. As I've grown it's grown. From the birthplace of my HTML skills to the refinement of my PHP Serubin.net has been my testing grounds. It also serves as a place to show of my level of commitment to my work.</p>
+								</section>
+
+							</div>
+							<div class="6u 12u(2)">
+							
+								<section>
+									<a href="#" class="image featured"><img src="images/projects/escapecraft.png" alt="" /></a>
+									<header>
+										<h3>Escapecraft</h3>
+									</header>
+									<p>Escapecraft is a Minecraft gaming community where I was able to work for 4 wonderful years. Here, I was introduced to System Administration and helped to maintain live gaming servers with very large playerbases. I was able to experience working within a team on large scale projects. I owe a lot of thanks to Escapecraft as they are a huge part of my technological growth.</p>
+								</section>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="6u 12u(2)">
+							
+								<section>
+									<a href="#" class="image featured"><img src="images/projects/mitll.png" alt="" /></a>
+									<header>
+										<h3>MIT Lincoln Lab</h3>
+									</header>
+									<p>MIT Lincoln Lab is a research facility that is under contract of the US Department of Defense. I was able to work on several pieces of large scale projects during my time here. Lincoln Lab is a place that fosters learning and growth, as such I was able to refine a lot of my Java and Javascript skills to aid my work.</p>
+								</section>
+
+							</div>
+							<div class="6u 12u(2)">
+							
+								<section>
+									<a href="#" class="image featured"><img src="images/projects/grouptones.png" alt="" /></a>
+									<header>
+										<h3>Grouptones</h3>
+									</header>
+									<p>Grouptones is an online website to help musicians connect to one another. Here I took the role of lead System Administrator. I was responsible for and managed all aspects of the website and server.</p>
+								</section>
+
+							</div>
+						</div>
+
+						<footer class="major">
+							<ul class="buttons">
+								<li><a href="#contact" class="button navscroll">Get started on your project!</a></li>
+							</ul>
+						</footer>
+					
+					</section>
+				</div>
+					
+			</article>
+
+		<!-- Contact-->
+			<section id="contact">
+			
+				<header>
+					<h2>Ready to get <strong>started</strong>?</h2>
+					<p>Send me an email</p>
+				</header>
+				<footer>
+					<?php
+						if(isset($_REQUEST['action'])){
+							$action=$_REQUEST['action'];
+							if ($action!="") {               /* send the submitted data */ 
+								$name=$_REQUEST['name']; 
+								$email=$_REQUEST['email']; 
+								$subject=$_REQUEST['subject']; 
+								$message=$_REQUEST['message']; 
+								if (($name=="")||($email=="")||($subject=="")||($message=="")) { 
+									echo "All fields are required, please be sure everything is filled out correctly."; 
+								} else {         
+									$from="From: $name<$email>\r\nReturn-path: $email"; 
+									$mail = mail("serubin@serubin.net", $subject, $message, $from); 
+									echo "<h2>Email sent! Send another?</h2>";
+								} 
+							}
+						}
+					?>
+					<form action="./#contact" method="POST" enctype="multipart/form-data">
+						<div class="row 50%">
+							<div class="6u 12u(3)">
+								<input type="text" name="name" placeholder="Name" />
+							</div>
+							<div class="6u 12u(3)">
+								<input type="text" name="email" placeholder="Email" />
+							</div>
+						</div>
+						<div class="row 50%">
+							<div class="12u">
+								<input type="text" name="subject" placeholder="Subject" />
+							</div>
+						</div>
+						<div class="row 50%">
+							<div class="12u">
+								<textarea name="message" placeholder="Message" rows="7"></textarea>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<ul class="buttons">
+									<li><input type="submit" class="special" value="Send Message" /></li>
+								</ul>
+							</div>
+						</div>
+						<input type="hidden" name="action" value="submit"> 
+					</form>
+				</footer>
+			
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<p class="copyright">
+					Copyright © 2012 Serubin.net
+					| Provided by <a href="http://Serubin.net" title="Serubin.net">Serubin.net</a>.
+					| <a href="https://secure.serubin.net/login/privacy" rel="nofollow" target="_blank" title="Privacy Policy">Privacy Policy</a>
+				</p>
+				<p class="copyright">Design: <a href="http://html5up.net">HTML5 UP</a></p>
+			
+			</footer>
+
+	</body>
+</html>
