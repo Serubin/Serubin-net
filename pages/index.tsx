e1dbb5f75989e1f253df1b721a2ebbd364c19b5d
Serubin <Serubin@serubin.net>
2021-09-19 14:11:48 -0700

    Clean slate - move over to nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Index.module.css'
+
+const Index: NextPage = () => {
+
+  return (
+    <div className={styles.container}>
+    </div>
+  )
+}
+
+export default Index
