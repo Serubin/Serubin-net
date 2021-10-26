7d285abbe46562f235587151c709dcda85ad4afb
Serubin <Serubin@serubin.net>
2021-10-25 21:46:11 -0700

    Update and add nav and about

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,18 @@ import styles from '../styles/Index.module.scss'
 import getStaticContent from '../lib/contentData';
 import Hero, { HeroProps } from '../components/hero'
 import { AboutData } from '../lib/types';
+import Nav from '../components/nav'
+import About from '../components/about';
 
 const Index: NextPage = ({ staticData }) => {
   const { name, tags, about }: AboutData = staticData.about;
 
   return (
     <>
+      <Nav />
       <div className={styles.container}>
           <Hero name={name} tags={tags} />
+          <About text={about} />
       </div>
     </>
   );
