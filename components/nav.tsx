279a86fdfbf010f38e9601af7d27a8393eb587fd
Serubin <Serubin@serubin.net>
2021-11-08 20:09:19 -0800

    Add nav bar

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,33 @@
 import { c } from '../lib/utils';
-import styles from '../styles/Nav.module.scss'
+import Link from 'next/link';
+import styles from '../styles/Nav.module.scss';
+import { SocialLink } from '../lib/types';
+import Logo from './logo';
 
-export default function Nav() {
+type PropsType = {
+  links: SocialLink[];
+}
+
+export default function Nav({ links }: PropsType) {
   return (
     <nav className={c(styles.navigation)}>
+      <section className={c(styles.left)}>
+        <Link href="/" passHref>
+          <Logo />
+        </Link>
+      </section>
+
+      <section className={c(styles.right)}>
+        <ul className={c(styles.linkList)}>
+          {links.map((link: SocialLink) => (
+            <li key={link.name}>
+              <a href={link.link} target="_blank" rel="noopener noreferrer">
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
     </nav>
   );
 }
