637f75b71646a0254fbf39f0b4b712421fcccc85
Serubin <Serubin@serubin.net>
2021-11-08 20:09:19 -0800

    Add nav bar

---
+++ components/logo.tsx
@@ -0,0 +1,9 @@
+import { c } from '../lib/utils';
+import styles from '../styles/Logo.module.scss'
+
+export default function Logo() {
+  return (
+    <span className={c(styles.logo)}>S</span>
+  );
+}
+
--- lib/types.ts
+++ lib/types.ts
@@ -2,4 +2,11 @@ export type AboutData = {
   name: string;
   tags: string[];
   about: string;
+  links: SocialLink[];
+};
+
+export type SocialLink = {
+  name: string;
+  icon: string;
+  link: string;
 };
