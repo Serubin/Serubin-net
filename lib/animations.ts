c7a75e6567b6a7794cb33fd312f2f0308a3a9b55
Serubin <Serubin@serubin.net>
2021-11-08 20:08:44 -0800

    Add animations

---
+++ lib/animations.ts
@@ -0,0 +1,52 @@
+const defaultDuration = 0.5;
+const defaultIntensity = 1;
+const defaultDelay = 0.3;
+const defaultEasing = 'easeIn';
+
+export const fadeUp = (speed = defaultDuration, intensity = defaultIntensity) => ({
+  initial: {
+    opacity: 0,
+    y: 16 * intensity,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  transition: {
+    duration: speed,
+    delay: defaultDelay,
+    ease: defaultEasing,
+  }
+});
+
+export const fadeDown = (speed = defaultDuration, intensity = defaultIntensity) => ({
+  initial: {
+    opacity: 0,
+    y: 16 * intensity * -1,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  transition: {
+    duration: speed,
+    delay: defaultDelay,
+    ease: defaultEasing,
+  }
+});
+
+export const fadeLeft = (speed = defaultDuration, intensity = defaultIntensity) => ({
+  initial: {
+    opacity: 0,
+    x: 16 * intensity * -1,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+  transition: {
+    duration: speed,
+    delay: defaultDelay,
+    ease: defaultEasing,
+  }
+});
