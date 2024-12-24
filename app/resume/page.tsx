cd88410acbd5f0dcb5923df5cbb28df2863e15f5
Solomon <serubin@serubin.net>
2024-12-23 19:28:58 -0800

    Update to next 15 (#22)
    * Update to next 15
    
    * Update docker image

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, NextPage } from 'next';
-import type { Resume as ResumeType } from '../../../resume/lib/types';
+import type { Resume as ResumeType } from '../../resume/lib/types';
 import getStaticContent from '../../lib/contentData';
-import Resume from '../../../resume/components/resume';
+import Resume from '../../resume/components/resume';
 
 type Props = {
   resume: ResumeType;
