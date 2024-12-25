e416891818dfde6c03328ae070faa018a340004b
Serubin <Serubin@serubin.net>
2024-12-25 12:35:20 -0500

    Remove unused page router

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.scss';
-
-import type { AppProps } from 'next/app';
-
-const SerubinNet = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-export default SerubinNet;
