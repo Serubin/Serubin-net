ca25209b64a91df1804c7f1fd6b7242110f9ea5b
Solomon <serubin@serubin.net>
2025-12-20 08:35:00 -0500

    feat(analytics): Add site analytics

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-};
+}
+
+const plausibleConfig = {
+  customDomain: 'https://analytics.serubin.net',
+  selfHosted: true,
+  trackOutboundLinks: true,
+  taggedEvents: true,
+}
+
+module.exports = withPlausibleProxy(plausibleConfig)(nextConfig);
